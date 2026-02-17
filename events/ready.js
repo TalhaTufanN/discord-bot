c55f42f3798348ac14a2a9362169e268f85c9bdd
Talha Tufan <talha.tufan@hotmail.com>
2026-02-17 14:45:53 +0300

    feat: auto-deploy update

--- events/ready.js
+++ events/ready.js
@@ -1,15 +1,14 @@
-
-const { Events, ActivityType } = require('discord.js');
+const { Events, ActivityType } = require("discord.js");
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
   execute(client) {
     console.log(`Hazır! ${client.user.tag} olarak giriş yapıldı`);
-    
+
     // Set bot activity
-    client.user.setActivity('müzik komutlarını', { type: ActivityType.Listening });
-    
+    client.user.setActivity("/çal", { type: ActivityType.Listening });
+
     console.log(`Bot ${client.guilds.cache.size} sunucuda aktif`);
   },
-}; 
+};
