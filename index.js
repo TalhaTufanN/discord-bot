05b531351becd633c3d7aa9daa42d71f2e179812
Talha Tufan <talha.tufan@hotmail.com>
2026-04-23 02:06:16 +0300

    feat: auto-deploy update

--- index.js
+++ index.js
@@ -37,22 +37,6 @@ const ffmpegPath = require("ffmpeg-static");
 client.distube = new DisTube(client, {
   emitNewSongOnly: true,
   plugins: [new SpotifyPlugin({}), new YtDlpPlugin({ update: false })],
-  // FFmpeg hızlandırma argümanları
-  // Daha güvenli ve uyumlu hızlandırma ayarları
-  ffmpeg: {
-    args: {
-      global: [
-        "-reconnect",
-        "1",
-        "-reconnect_streamed",
-        "1",
-        "-reconnect_delay_max",
-        "5",
-      ],
-      input: ["-analyzeduration", "100000", "-probesize", "100000"], // Tam 0 yerine çok küçük bir değer (0.1sn)
-      output: ["-fflags", "nobuffer", "-flags", "low_delay"],
-    },
-  },
 });
 
 // Load commands
