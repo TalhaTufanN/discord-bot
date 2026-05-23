bfe7d0bf5f7078cda0cf9c5048392f5668bcaa04
Talha Tufan <talha.tufan@hotmail.com>
2026-05-23 23:29:24 +0300

    feat: auto-deploy update

--- index.js
+++ index.js
@@ -133,12 +133,12 @@ client.distube = new DisTube(client, {
   emitNewSongOnly: true,
   ffmpeg: {
     args: {
-      input: [
-        "-reconnect", "1",
-        "-reconnect_at_eof", "1",
-        "-reconnect_streamed", "1",
-        "-reconnect_delay_max", "5"
-      ]
+      input: {
+        "-reconnect": 1,
+        "-reconnect_at_eof": 1,
+        "-reconnect_streamed": 1,
+        "-reconnect_delay_max": 5
+      }
     }
   },
   plugins: [
