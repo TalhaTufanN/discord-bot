058a49a8ad6acfb016dd16e6233ce8c06c959865
Talha Tufan <talha.tufan@hotmail.com>
2026-05-23 23:27:19 +0300

    feat: auto-deploy update

--- index.js
+++ index.js
@@ -131,6 +131,16 @@ youtubePlugin.getStreamURL = getStreamURLWithFallback;
 const ffmpegPath = require("ffmpeg-static");
 client.distube = new DisTube(client, {
   emitNewSongOnly: true,
+  ffmpeg: {
+    args: {
+      input: [
+        "-reconnect", "1",
+        "-reconnect_at_eof", "1",
+        "-reconnect_streamed", "1",
+        "-reconnect_delay_max", "5"
+      ]
+    }
+  },
   plugins: [
     new SpotifyPlugin({}),
     ytDlpPlugin,
